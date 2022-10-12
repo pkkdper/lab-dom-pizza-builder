a926ff640e92c73dcb85b1ce2f84f7c0cd72714e
pkkdpr <oliwia.giercza@gmail.com>
2022-10-12 05:58:09 -0500

    done

--- js/index.js
+++ js/index.js
@@ -43,19 +43,40 @@ function renderPepperoni() {
 }
 
 function renderMushrooms() {
-  // Iteration 1: set the visibility of `<section class="mushroom">`
+  document.querySelectorAll('.mushroom').forEach((oneMushroom) => {
+    if (state.green - pepper) {
+      oneMushroom.style.visibility = 'visible';
+    } else {
+      oneMushroom.style.visibility = 'hidden';
+    }
+  });
 }
 
 function renderGreenPeppers() {
-  // Iteration 1: set the visibility of `<section class="green-pepper">`
+  document.querySelectorAll('.green-pepper').forEach((greenPepper) => {
+    if (state.greenPepper) {
+      greenPepper.style.visibility = 'visible';
+    } else {
+      greenPepper.style.visibility = 'hidden';
+    }
+  });
 }
 
 function renderWhiteSauce() {
-  // Iteration 2: add/remove the class "sauce-white" of `<section class="sauce">`
+  let sauce = document.querySelector('.sauce');
+  if (!state.whiteSauce) {
+    sauce.classList.remove('sauce-white');
+  } else {
+    sauce.classList.add('sauce-white');
+  }
 }
-
 function renderGlutenFreeCrust() {
-  // Iteration 2: add/remove the class "crust-gluten-free" of `<section class="crust">`
+  let crust = document.querySelectorAll('.crust');
+  if (!state.glutenFreeCrust) {
+    crust.classList.remove('crust-gluten-free');
+  } else {
+    crust.classList.add('crust-gluten-free');
+  }
 }
 
 function renderButtons() {
@@ -69,10 +90,12 @@ function renderPrice() {
 renderEverything();
 
 // Iteration 1: Example of a click event listener on `<button class="btn btn-pepperoni">`
-document.querySelector('.btn.btn-pepperoni').addEventListener('click', function () {
-  state.pepperoni = !state.pepperoni;
-  renderEverything();
-});
+document
+  .querySelector('.btn.btn-pepperoni')
+  .addEventListener('click', function () {
+    state.pepperoni = !state.pepperoni;
+    renderEverything();
+  });
 
 // Iteration 1: Add click event listener on `<button class="btn btn-mushrooms">`
 
@@ -81,3 +104,12 @@ document.querySelector('.btn.btn-pepperoni').addEventListener('click', function 
 // Iteration 2: Add click event listener on `<button class="btn btn-sauce">`
 
 // Iteration 2: Add click event listener on `<button class="btn btn-crust">`
+/*
+
+function renderButtons() {
+  // Iteration 3: add/remove the class "active" of each `<button class="btn">`
+}
+
+function renderPrice() {
+  // Iteration 4: change the HTML of `<aside class="panel price">`
+}*/
